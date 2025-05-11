9ff533ef0996fc96fe5f4bb66e24543b823fd213
ER\Ujjaval <ujjaval.vyas@zoodmall.com>
2025-05-11 17:09:42 +0530

    1st push

---
+++ autoTest-1/src/auto1.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class auto1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+		
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		//driver.get("https://www.google.com");
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("mobile");
+		driver.findElement(By.xpath("//*[@id=\"gh-search-btn\"]")).click();
+
+		
+	}
+
+}
